a54d60a08badad97bd7175a397f39469efd4e155
Yurii Surzhykov <yurii.surzhykov@gcpsoftware.com>
2021-03-06 00:44:22 +0200

    Initial commit

---
+++ app/src/main/java/com/yuriysurzhikov/lab2/SecondActivity.kt
@@ -0,0 +1,13 @@
+package com.yuriysurzhikov.lab2
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import de.hdodenhof.circleimageview.CircleImageView
+
+class SecondActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+        val image: CircleImageView
+    }
+}
